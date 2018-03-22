18417e403d85954c516a3230eb1be05f68dc48e1
gully <igully@gmail.com>
2018-03-21 17:07:02 -0700

    Batch scripts for grid.py and pca.py

--- code/batch_config.py
+++ code/batch_config.py
@@ -4,17 +4,18 @@ import yaml
 import numpy as np
 import h5py
 
-ms = list(range(100, 112)) + list(range(113, 126))
+ms = list(range(105, 125))
+#ms = list(range(105, 106))
 
-os.chdir(os.path.expandvars('$xveganx/sf/Anon1/'))
+os.chdir(os.path.expandvars('$xveganx/sf/V827Tau/'))
 
 for m in ms:
 
-    dat_name = os.path.expandvars('$xveganx/data/IGRINS/reduced/Anon1_20141118_{:03d}.hdf5'.format(m))
+    dat_name = os.path.expandvars('$xveganx/data/IGRINS/reduced/V827_IGRINS_20141121_m{:03d}.hdf5'.format(m))
     path_out = 'm{:03d}/'.format(m)
     sf_out = 'm{:03d}/config.yaml'.format(m)
 
-    f = open("m112/config.yaml")
+    f = open("m104/output/mix_emcee/run01/config.yaml")
     config = yaml.load(f)
     f.close()
 
@@ -22,22 +23,23 @@ for m in ms:
     wls = f['wls'][:]
     f.close()
 
-    config['data']['files'] = ['$xveganx/data/IGRINS/reduced/Anon1_20141118_{:03d}.hdf5'.format(m)]
-    config['grid']['hdf5_path'] = '$xveganx/sf/Anon1/m{:03d}/libraries/PHOENIX_IGRINS_m{:03d}_Teff2700-4500.hdf5'.format(m,m)
+    config['data']['files'] = ['$xveganx/data/IGRINS/reduced/V827_IGRINS_2014112{:03d}.hdf5'.format(m)]
+    config['grid']['hdf5_path'] = '$xveganx/sf/V827Tau/m{:03d}/libraries/PHOENIX_IGRINS_m{:03d}_Teff2700-4500.hdf5'.format(m,m)
     lb, ub = int(np.floor(wls[0])), int(np.ceil(wls[-1]))
 
     config['grid']['wl_range'] = [lb, ub]
-    config['PCA']['path'] = '$xveganx/sf/Anon1/m{:03d}/PHOENIX_IGRINS_H_PCA_Teff2700-4500.hdf5'.format(m)
+    config['PCA']['path'] = '$xveganx/sf/V827Tau/m{:03d}/PHOENIX_IGRINS_H_PCA_Teff2700-4500.hdf5'.format(m)
     config['data']['instruments'] =['IGRINS_H']
-    config['Theta_priors']= '$xveganx/sf/Anon1/m{:03d}/user_prior.py'.format(m)
+    config['Theta_priors']= '$xveganx/sf/V827Tau/m{:03d}/user_prior.py'.format(m)
 
     os.makedirs(path_out, exist_ok=True)
     with open(sf_out, mode='w') as outfile:
         outfile.write(yaml.dump(config))
         print('wrote to {}'.format(path_out))
 
-#for m in ms:
-#    os.chdir("m{:03d}".format(m))
-#    os.system('mkdir libraries &')
-#    os.system('$Starfish/scripts/grid.py --create > grid.out &')
-#    os.chdir("..")
+for m in ms:
+    os.chdir("m{:03d}".format(m))
+    os.system('mkdir libraries &')
+    os.system('$Starfish/scripts/grid.py --create > grid.out &')
+    os.system('cp $xveganx/sf/V827Tau/m104/user_prior.py .')
+    os.chdir("..")
--- code/batch_pca.py
+++ code/batch_pca.py
@@ -4,25 +4,20 @@ import yaml
 import numpy as np
 import h5py
 
-ms = list(range(100, 112)) + list(range(113, 126))
+ms = list(range(106, 125))
+#ms = list(range(105, 106))
 
-os.chdir(os.path.expandvars('$xveganx/sf/Anon1/'))
-
-#for ch in chs:
-#    os.chdir("K_ch{:03d}".format(ch))
-#    os.system('mkdir libraries &')
-#    os.system('grid.py --create > grid.out &')
-#    os.chdir("..")
+os.chdir(os.path.expandvars('$xveganx/sf/V827Tau/'))
 
 for m in ms:
     os.chdir("m{:03d}".format(m))
     os.system('$Starfish/scripts/pca.py --create > pca_create.out')
-    os.system('time $Starfish/scripts/pca.py --optimize=emcee --samples=40  > pca_optimize.out')
+    os.system('time $Starfish/scripts/pca.py --optimize=emcee --samples=3  > pca_optimize.out')
     os.system('time $Starfish/scripts/pca.py --store --params=emcee > pca_store.out')
     os.system('mkdir output')
     os.system('mkdir output/mix_emcee')
     os.system('mkdir output/mix_emcee/run01')
-    os.system('cp $xveganx/sf/Anon1/m112/output/mix_emcee/run01/s0_o0phi.json output/mix_emcee/run01/')
-    os.system('cp $xveganx/sf/Anon1/m112/user_prior.py .')
+    os.system('cp $xveganx/sf/V827Tau/m104/output/mix_emcee/run01/s0_o0phi.json output/mix_emcee/run01/')
+    #os.system('cp $xveganx/sf/V827Tau/m104/user_prior.py .')
     os.system('cp config.yaml output/mix_emcee/run01/')
     os.chdir("..")
